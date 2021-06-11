7bcd18de2912fcf53cd555546b3be6f6420a6974
Esa Koskinen <esakoskin2@gmail.com>
2021-06-11 21:46:32 +0900

    add game's initialization time to state

--- src/core/time/time.ts
+++ src/core/time/time.ts
@@ -1,12 +1,15 @@
 export type Time = {
+  start: number
   now: number
   elapsed: number
 }
 
+const initialTimeSeconds = Date.now() / 1000
 export const getTime = (sinceLastTime?: Time): Time => {
   const secondsSinceEpoch = Date.now() / 1000
   return {
+    start: initialTimeSeconds,
     now: secondsSinceEpoch,
-    elapsed: sinceLastTime != null ? secondsSinceEpoch - sinceLastTime.now : 0
+    elapsed: sinceLastTime != null ? secondsSinceEpoch - sinceLastTime.now : 0,
   }
 }
