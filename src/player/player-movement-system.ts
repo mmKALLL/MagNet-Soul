3c63fce62423b71b2ea9c317567810ca39ce34fb
Esa Koskinen <esakoskin2@gmail.com>
2021-06-12 21:43:46 +0900

    adjust player physics
    - makes the movement feel more controlled and less chaotic

--- src/player/player-movement-system.ts
+++ src/player/player-movement-system.ts
@@ -4,21 +4,26 @@ import Physics from '../core/physics/physics'
 import * as Player from '../player/player'
 import * as Keyboard from '../core/input/keyboard'
 import Vector, { Vectors } from '../core/math/vector'
+import { clamp } from '../core/math/numeric'
 
 export const PlayerMovementSystem = System.create<MyState>((game, time) => {
   const body = game.state.physicsBodies.get(Player.ID)!
   const keyboard = game.input.keyboard
 
-  const direction = new Vector(0, 0)
+  const speedMultiplier = 2.35
+  let xSpeed = 0
   if (Keyboard.isDown(keyboard, 'a') || Keyboard.isDown(keyboard, 'ArrowLeft')) {
-    direction.add(Vectors.left())
+    xSpeed -= speedMultiplier
   }
 
   if (Keyboard.isDown(keyboard, 'd') || Keyboard.isDown(keyboard, 'ArrowRight')) {
-    direction.add(Vectors.right())
+    xSpeed += speedMultiplier
   }
 
-  Physics.Body.applyForce(body, body.position, direction.multiplyScalar(0.0011))
+  Physics.Body.setVelocity(body, {
+    x: clamp(body.velocity.x * 0.75 + xSpeed, -speedMultiplier, speedMultiplier),
+    y: body.velocity.y,
+  })
 
   if (
     (Keyboard.isDown(keyboard, ' ') ||
@@ -26,6 +31,6 @@ export const PlayerMovementSystem = System.create<MyState>((game, time) => {
       Keyboard.isDown(keyboard, 'ArrowUp')) &&
     Math.abs(body.velocity.y) < 0.01
   ) {
-    Physics.Body.applyForce(body, body.position, Vectors.up().multiplyScalar(0.028))
+    Physics.Body.applyForce(body, body.position, Vectors.up().multiplyScalar(0.025))
   }
 })
