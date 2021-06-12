aa1e1a63786bf370f4748bc6514b99ea62d79c25
Esa Koskinen <esakoskin2@gmail.com>
2021-06-12 18:59:48 +0900

    reset camera scale

--- src/systems/camera-system.ts
+++ src/systems/camera-system.ts
@@ -4,7 +4,7 @@ import { MyState } from '../main'
 
 export const CameraSystem = System.create<MyState>((game, time) => {
   game.state.cameras.forEach((id, position) => {
-    game.state.stage.scale.set(2)
+    game.state.renderStage.scale.set(1.1)
     // const sprite = game.state.sprites.get(id)
     // if (sprite) {
     //   sprite.position.x = body.position.x
