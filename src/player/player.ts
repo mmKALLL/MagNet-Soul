cb40cc8869c0e78a8ec9c5827c0b86e8ec7bde62
Esa Koskinen <esakoskin2@gmail.com>
2021-06-12 18:59:21 +0900

    prettier fixes, move player debug into player.ts

--- src/player/player.ts
+++ src/player/player.ts
@@ -1,10 +1,11 @@
 import * as PIXI from 'pixi.js'
 import { Entity } from '../arch/arch'
 import { assets } from '../assets'
+import Vector from '../core/math/vector'
 import Physics from '../core/physics/physics'
-import { MyState } from "../main"
+import { MyState } from '../main'
 
-export const ID = "player"
+export const ID = 'player'
 
 export const create = (game: MyState): Entity.ID => {
   const playerId = game.entities.create(ID)
@@ -18,10 +19,11 @@ export const create = (game: MyState): Entity.ID => {
     frictionStatic: 0,
     restitution: 0.4,
     inertia: Infinity,
-    inverseInertia: 0
+    inverseInertia: 0,
   })
   game.physicsBodies.set(playerId, body)
   Physics.World.addBody(game.physicsWorld, body)
+  Physics.Body.setPosition(body, new Vector(100, 100)) // debug
 
   const sprite = PIXI.Sprite.from(assets.character)
   sprite.anchor.set(0.5, 0.5)
