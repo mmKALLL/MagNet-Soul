57901bacb9259a3a2a700253df665855a384e8a9
Esa Koskinen <esakoskin2@gmail.com>
2021-06-12 20:31:06 +0900

    add ring asset
    - maybe for bullets or pickups

--- src/assets.ts
+++ src/assets.ts
@@ -3,4 +3,5 @@ export const assets = {
   tileset1: require('./assets/tiles/starry-night-by-quintino/Tileset.png'),
   tileset2: require('./assets/tiles/starry-night-by-quintino/clouds.png'),
   tileset3: require('./assets/tiles/starry-night-by-quintino/background_sky.png'),
+  ring46: require('./assets/sprites/icons/Accessories and Armor - Clockwork Raven Studios/16x16/tile046.png'),
 } as const
