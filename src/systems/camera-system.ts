87550ed512ab8b994087863da750da92089f21b5
shokoalien <ybuchi3168@gmail.com>
2021-06-13 18:27:08 +0900

    Fix camera bounds

--- src/systems/camera-system.ts
+++ src/systems/camera-system.ts
@@ -1,9 +1,11 @@
 import * as PIXI from 'pixi.js'
 import * as System from '../arch/system'
+import { clamp } from '../core/math/numeric'
 import { MyState } from '../main'
 
 const zoom = 2.5
 const x_base = 400
+const stage_width = 1600
 
 export const CameraSystem = System.create<MyState>(
   () => {},
@@ -12,8 +14,9 @@ export const CameraSystem = System.create<MyState>(
       game.state.renderStage.scale.set(zoom)
       const playerX = game.state.physicsBodies.get('player')?.position.x
       if (playerX) {
+        const calcX = -zoom * playerX + x_base
         game.state.renderStage.position.x =
-          -zoom * game.state.physicsBodies.get('player')!.position.x + x_base
+          clamp(calcX, stage_width * -zoom + window.innerWidth, 0)
       }
     })
   }
