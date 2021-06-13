c9625c8a1a37153bcf6187642db9b0bedc9bdf9b
Gabriel Lumbi <gabriel.lumbi@gmail.com>
2021-06-13 22:15:02 +0900

    Make jump sensor more lenient

--- src/player/player-movement-system.ts
+++ src/player/player-movement-system.ts
@@ -5,6 +5,7 @@ import * as Player from '../player/player'
 import * as Keyboard from '../core/input/keyboard'
 import Vector, { Vectors } from '../core/math/vector'
 import { clamp } from '../core/math/numeric'
+import { CollisionCategories } from '../collision-categories'
 
 export const PlayerMovementSystem = System.create<MyState>(
   () => {},
@@ -47,15 +48,21 @@ export const PlayerMovementSystem = System.create<MyState>(
       (Keyboard.isDown(keyboard, ' ') ||
         Keyboard.isDown(keyboard, 'Spacebar') ||
         Keyboard.isDown(keyboard, 'ArrowUp')) &&
-      Math.abs(body.velocity.y) < 0.01
+      Math.abs(body.velocity.y) < 0.05
     ) {
-      const terrain = game.state.physicsWorld.bodies.filter((body) => body.label == 'terrain')
-      const belowPlayer = {
-        x: body.position.x,
-        y: body.position.y + 11,
-      }
-      const terrainBelowPlayer = Physics.Query.point(terrain, belowPlayer)
-      if (terrainBelowPlayer.length > 0) {
+      const terrain = game.state.physicsWorld.bodies
+        .map((body) => ({ body, category: body.collisionFilter?.category ?? 0 }))
+        .filter(({ category }) =>
+          (category & CollisionCategories.level) !== 0 ||
+          (category & CollisionCategories.enemy) !== 0
+        )
+        .map(({ body }) => body)
+      const below = Physics.Query.point(terrain, { x: body.position.x, y: body.position.y + 11, })
+      const left = Physics.Query.point(terrain, { x: body.position.x-5, y: body.position.y + 11, })
+      const right = Physics.Query.point(terrain, { x: body.position.x+5, y: body.position.y + 11, })
+      const collidingTerrain = below.concat(left).concat(right)
+      const isGrounded = collidingTerrain.length > 0
+      if (isGrounded) {
         game.state.playerAnimState.next = 'jump'
         Physics.Body.applyForce(body, body.position, Vectors.up().multiplyScalar(0.025))
       }
