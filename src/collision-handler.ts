b02928995f9890762e89e5f4ea8952708f8a18a4
Esa Koskinen <esakoskin2@gmail.com>
2021-06-13 20:28:17 +0900

    prettier formatting

--- src/collision-handler.ts
+++ src/collision-handler.ts
@@ -3,7 +3,11 @@ import Physics from './core/physics/physics'
 import { MyState } from './main'
 
 export type CollisionPredicate = (body: Physics.Body) => boolean
-export type CollisionCallback = (body: Physics.Body, other: Physics.Body, pair: Physics.IPair) => void
+export type CollisionCallback = (
+  body: Physics.Body,
+  other: Physics.Body,
+  pair: Physics.IPair
+) => void
 
 export const handleCollisions = (
   game: GameState<MyState>,
@@ -11,12 +15,12 @@ export const handleCollisions = (
   callback: CollisionCallback
 ) => {
   Physics.Events.on(game.state.physicsEngine, 'collisionStart', (collisions) => {
-      for (const pair of collisions.pairs) {
-        if (predicate(pair.bodyA)) {
-          callback(pair.bodyA, pair.bodyB, pair)
-        } else if (predicate(pair.bodyB)) {
-          callback(pair.bodyB, pair.bodyA, pair)
-        }
+    for (const pair of collisions.pairs) {
+      if (predicate(pair.bodyA)) {
+        callback(pair.bodyA, pair.bodyB, pair)
+      } else if (predicate(pair.bodyB)) {
+        callback(pair.bodyB, pair.bodyA, pair)
+      }
     }
   })
 }
