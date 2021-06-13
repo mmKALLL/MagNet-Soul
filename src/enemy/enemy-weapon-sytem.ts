9856ed46ae4fad06ce60a32bb1a590f4abfa37af
Esa Koskinen <esakoskin2@gmail.com>
2021-06-14 00:50:48 +0900

    prettier changes

--- src/enemy/enemy-weapon-sytem.ts
+++ src/enemy/enemy-weapon-sytem.ts
@@ -6,16 +6,20 @@ import Physics from '../core/physics/physics'
 import * as EnemyBullet from '../enemy/enemy-bullet'
 
 export const EnemyWeaponSystem = System.create<MyState>(
-  () => { },
+  () => {},
   (game, time) => {
     game.state.weapon.forEach((id, weapon) => {
       if (game.state.entityType.get(id) == 'enemy') {
         if (time.now > weapon.lastTimeFired + weapon.fireRate) {
           const enemyBody = game.state.physicsBodies.get(id)
           const playerBody = game.state.physicsBodies.get(Player.ID)
-          if (!enemyBody || !playerBody) { return }
-          const distanceToPlayer = Physics.Vector.magnitude(Physics.Vector.sub(playerBody.position, enemyBody.position))
-          if (distanceToPlayer < 200) {
+          if (!enemyBody || !playerBody) {
+            return
+          }
+          const distanceToPlayer = Physics.Vector.magnitude(
+            Physics.Vector.sub(playerBody.position, enemyBody.position)
+          )
+          if (distanceToPlayer < 13 * 16) {
             weapon.lastTimeFired = time.now
             const direction = Vectors.left()
             const position = new Vector(enemyBody.position.x, enemyBody.position.y)
@@ -26,7 +30,7 @@ export const EnemyWeaponSystem = System.create<MyState>(
             }
             game.state.ttl.set(bulletId, {
               createdAt: time.now,
-              expiresIn: 2
+              expiresIn: 2,
             })
           }
         }
