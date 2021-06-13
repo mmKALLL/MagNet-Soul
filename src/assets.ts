955a93fe781d9b007a7f07db836f3e7c15ebd777
Esa Koskinen <esakoskin2@gmail.com>
2021-06-13 13:11:04 +0900

    add full stage passive layer as single asset

--- src/assets.ts
+++ src/assets.ts
@@ -5,4 +5,5 @@ export const assets = {
   tileset2: require('./assets/tiles/starry-night-by-quintino/clouds.png'),
   tileset3: require('./assets/tiles/starry-night-by-quintino/background_sky.png'),
   ring46: require('./assets/sprites/icons/Accessories and Armor - Clockwork Raven Studios/16x16/tile046.png'),
+  baseBackground1: require('./assets/tiles/html-game-test.png'),
 } as const
