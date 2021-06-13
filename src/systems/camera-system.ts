055a0d970f2f6aaf5403a0a78e67cad9247668f0
Esa Koskinen <esakoskin2@gmail.com>
2021-06-13 11:45:36 +0900

    prettier reformatting

--- src/systems/camera-system.ts
+++ src/systems/camera-system.ts
@@ -6,11 +6,12 @@ const zoom = 2.5
 const x_base = 250
 
 export const CameraSystem = System.create<MyState>(
-  () => { },
+  () => {},
   (game, time) => {
     game.state.cameras.forEach((id, position) => {
       game.state.renderStage.scale.set(zoom)
-      game.state.renderStage.position.x = -zoom * game.state.physicsBodies.get('player')!.position.x + x_base
+      game.state.renderStage.position.x =
+        -zoom * game.state.physicsBodies.get('player')!.position.x + x_base
       // const sprite = game.state.sprites.get(id)
       // if (sprite) {
       //   sprite.position.x = body.position.x
--- src/systems/draw-sprites-system.ts
+++ src/systems/draw-sprites-system.ts
@@ -2,7 +2,7 @@ import * as System from '../arch/system'
 import { MyState } from '../main'
 
 export const DrawSpritesSystem = System.create<MyState>(
-  () => { },
+  () => {},
   (game, time) => {
     game.state.physicsBodies.forEach((id, body) => {
       const sprite = game.state.sprites.get(id)
@@ -14,7 +14,7 @@ export const DrawSpritesSystem = System.create<MyState>(
     })
     const player_x = game.state.physicsBodies.get('player')!.position.x
     game.state.backgrounds.forEach((id, obj) => {
-      const {sprite, original_x, parallaxX} = obj
+      const { sprite, original_x, parallaxX } = obj
       if (parallaxX > 0) {
         const adjust_x = player_x * (1 - parallaxX)
         sprite.position.x = original_x + adjust_x
@@ -22,4 +22,3 @@ export const DrawSpritesSystem = System.create<MyState>(
     })
   }
 )
-
